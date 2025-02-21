61453c58f7ada486a18e0ddd601ab0e3dba4a90b
Rohit <Rohitsmali9421@gmail.com>
2025-02-21 16:25:33 +0530

    ChatApp

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cors = require("cors");
+const connectMongoDB = require("./config/connection");
+const UserRouter = require("./Routes/User");
+const MessageRouter = require("./Routes/Message");
+const cookieParser = require("cookie-parser");
+const { app, server } = require("./Socket/socket");
+const express = require("express");
+const port = process.env.PORT || 8000;
+const URI = process.env.MONGODB_URI;
+connectMongoDB(URI);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+//Midelewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+app.use("/api/user", UserRouter);
+app.use("/api/message", MessageRouter);
+
+server.listen(port, () => {
+  console.log("server Started");
+});
